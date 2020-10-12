60df11c66bc06d9519e5ed952fba0a5b6d20e5ca
Jack Sari <janasarii@gmail.com>
2020-10-12 01:56:33 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// eslint-disable-next-line no-undef
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="conatiner">
+      <header>
+        <h1>Gasto Semanal</h1>
       </header>
     </div>
   );
